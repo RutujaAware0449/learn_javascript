5dc5e0dcccb07d145d176c932d61856ce52d3f87
Rutuja Aware <112408342+RutujaAware0449@users.noreply.github.com>
2023-10-22 10:11:19 +0000

    conversion of datatypes in javascript

---
+++ conversion.js
@@ -0,0 +1,27 @@
+let score= 33;
+console.log(typeof score);
+console.log(typeof score);
+let score1= "rutuja";
+console.log(typeof score1);
+console.log(typeof score1);
+let valueInNumber= Number(score1);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+//"33"=> 33
+// "rutuja"=> NaN
+// true => 1
+
+let islogged= 1;
+let booleanIs= Boolean(islogged)
+console.log(booleanIs);
+//0=>false
+//1=>true
+//""=>false
+//"rutuj"=>r=true
+
+let someNumber=99;
+let stringconver=String(someNumber);
+console.log(someNumber);
+console.log(typeof someNumber);
